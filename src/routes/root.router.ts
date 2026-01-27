1caacb741b2330fac92fcc79b47f9a53954341db
Jonas Olson <jonas.olson@gmail.com>
2026-01-27 10:21:41 +0100

    Added routs

--- src/routes/root.router.ts
+++ src/routes/root.router.ts
@@ -7,10 +7,32 @@ export const rootRouter = express.Router();
  * GET /
  */
 rootRouter.get("/", (_req, res) => {
-	res.send({ status: "success", data: { message: "But first, let me take a selfie 🤳 https://www.youtube.com/watch?v=kdemFfbS5H0" }});
+	res.send({
+		status: "success",
+		data: {
+			message:
+				"But first, let me take a selfie 🤳 https://www.youtube.com/watch?v=kdemFfbS5H0",
+		},
+	});
 });
 
 /**
  * [EXAMPLE] /resource
  */
 // rootRouter.use("/resource", resourceRouter);
+
+// Photos router
+rootRouter.use("/photos", photosRouter);
+
+//Albums router
+rootRouter.use("/albums", albumsRouter);
+
+// User router
+
+rootRouter.use("/user", userRouter);
+
+// Auth router
+rootRouter.use(authRouter);
+
+// Profile router
+rootRouter.use("/profile", profileRouter);
