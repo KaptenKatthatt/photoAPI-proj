4de169a4dccc49439a888e797e8545c8cbbfbd5e
Jonas Olson <jonas.olson@gmail.com>
2026-01-29 15:41:09 +0100

    [wip]Created and signed access and refresh tokens

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,19 +6,20 @@ import { Request, Response } from "express";
 import type { CreateUserData } from "../types/User.types.ts";
 import Debug from "debug";
 import jwt from "jsonwebtoken";
-import type { JWTAccessTokenPayload } from "../types/JWT.types.ts";
 import { matchedData } from "express-validator";
 import { handlePrismaError } from "../lib/handlePrismaError.ts";
 import { createUser, getUserByEmail } from "../services/user.service.ts";
+import type { JWTAccessTokenPayload } from "../types/JWT.types.ts";
+import { StringValue } from "ms";
 
 // Create a debug instance
 const debug = Debug("prisma-books:auth_controller");
 
 // Environment variables
-const ACCESS_TOKEN_LIFETIME = process.env.ACCESS_TOKEN_LIFETIME || "15m";
+const ACCESS_TOKEN_LIFETIME = (process.env.ACCESS_TOKEN_LIFETIME as StringValue) || "15m";
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
-const REFRESH_TOKEN_LIFETIME = process.env.REFRESH_TOKEN_LIFETIME || "1d";
+const REFRESH_TOKEN_LIFETIME = (process.env.REFRESH_TOKEN_LIFETIME as StringValue) || "1d";
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 const SALT_ROUNDS = Number(process.env.SALT_ROUNDS) || 10;
@@ -55,7 +56,6 @@ const registerUser = async (req: Request, res: Response) => {
 		res.status(201).send({
 			status: "success",
 			data: { user },
-
 		});
 	} catch (error) {
 		handlePrismaError(res, error);
@@ -86,7 +86,7 @@ export const loginUser = async (req: Request, res: Response) => {
 	}
 	console.log("Login successful");
 
-	// Create JWT payload
+	// Create JWT access token payload
 	const payload: JWTAccessTokenPayload = {
 		sub: String(user.id),
 		firstName: user.first_name,
@@ -94,7 +94,18 @@ export const loginUser = async (req: Request, res: Response) => {
 		email: user.email,
 	};
 
-
 	// Sign payload with access-token secret
-	const accessToken =jwt.sign(payoad
+	const accessToken = jwt.sign(payload, ACCESS_TOKEN_SECRET, {
+		expiresIn: ACCESS_TOKEN_LIFETIME as StringValue,
+	});
+
+	// Create JWT refresh token payload
+	const refreshTokenPayload = {
+		sub: String(user.id),
+	};
+
+	// Sign payload with refresh-token secret
+	const refreshToken = jwt.sign(refreshTokenPayload, REFRESH_TOKEN_SECRET, {
+		expiresIn: REFRESH_TOKEN_LIFETIME,
+	});
 };
