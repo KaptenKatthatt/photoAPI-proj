84ce32f065a7d302c8a53e40586b86fc05553171
Jonas Olson <jonas.olson@gmail.com>
2026-02-01 13:01:09 +0100

    Refactor user validation rules to remove redundant string checks and improve clarity

--- src/rules/user.rules.ts
+++ src/rules/user.rules.ts
@@ -6,28 +6,18 @@ export const createUserRules = [
 		.trim()
 		.isEmail()
 		.withMessage("Email must be a valid email address")
+		.bail()
 		.custom(validateEmailDoesNotExist),
 
-	body("password")
-		.isString()
-		.withMessage("Password must be a string")
-		.bail()
-		.isLength({ min: 6 })
-		.withMessage("Password must be at least 6 characters long"),
+	body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters"),
 
 	body("first_name")
-		.isString()
-		.withMessage("First name must be a string")
-		.bail()
 		.trim()
 		.isLength({ min: 3 })
 		.withMessage("First name must be at least 3 characters"),
 
 	body("last_name")
 		.trim()
-		.isString()
-		.withMessage("Last name must be a string")
-		.bail()
 		.isLength({ min: 3 })
 		.withMessage("Last name must be at least 3 characters"),
 ];
@@ -35,18 +25,12 @@ export const createUserRules = [
 export const updateUserRules = [
 	body("first_name")
 		.optional()
-		.isString()
-		.withMessage("First name must be a string")
-		.bail()
 		.trim()
 		.isLength({ min: 3, max: 191 })
 		.withMessage("First name must be between 3 and 191 characters"),
 
 	body("last_name")
 		.optional()
-		.isString()
-		.withMessage("Last name must be a string")
-		.bail()
 		.trim()
 		.isLength({ min: 3, max: 191 })
 		.withMessage("Last name must be between 3 and 191 characters"),
@@ -60,9 +44,6 @@ export const updateUserRules = [
 
 	body("password")
 		.optional()
-		.isString()
-		.withMessage("Password must be a string")
-		.bail()
 		.isLength({ min: 6 })
 		.withMessage("Password must be at least 6 characters long"),
 ];
