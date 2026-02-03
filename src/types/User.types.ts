e18ec765322b408630cacd6f2682f413f728f571
Jonas Olson <jonas.olson@gmail.com>
2026-02-03 14:31:34 +0100

    Remove unused UserId type from User.types.ts

--- src/types/User.types.ts
+++ src/types/User.types.ts
@@ -7,5 +7,3 @@ import { User } from "../../generated/prisma/client.ts";
 export type CreateUserData = Omit<User, "id">;
 
 export type UpdateUserData = Partial<CreateUserData>;
-
-export type UserId = Pick<User, "id">;
