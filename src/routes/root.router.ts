315a52e3f7723133adfc8ed125fa1f1b20711689
Jonas Olson <jonas.olson@gmail.com>
2026-02-03 18:53:12 +0100

    Root to 200 data null

--- src/routes/root.router.ts
+++ src/routes/root.router.ts
@@ -11,13 +11,7 @@ export const rootRouter = express.Router();
  * GET /
  */
 rootRouter.get("/", (_req, res) => {
-	res.status(200).send({
-		status: "success",
-		data: {
-			message:
-				"Cat: We're all mad here. I'm mad. You're mad. \nHow do you know I'm mad? said Alice. \nYou must be, said the Cat, or you wouldn't have come here..., https://www.youtube.com/watch?v=fhmgbLI9CyE",
-		},
-	});
+	res.status(200).send({ status: "success", data: null });
 });
 
 // Photos router
